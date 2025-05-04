282bb45926a30512b4be10bff12baeb034eab9e0
alisher134 <alisherrakhmanov134@gmail.com>
2025-05-04 12:47:37 +0500

    first commit

---
+++ src/app.module.ts
@@ -0,0 +1,19 @@
+import { Module } from "@nestjs/common";
+import { ConfigModule } from "@nestjs/config";
+import { AuthModule } from "./auth/auth.module";
+import { MediaModule } from "./media/media.module";
+import { SettingsModule } from "./settings/settings.module";
+import { StatisticsModule } from "./statistics/statistics.module";
+import { UserModule } from "./user/user.module";
+
+@Module({
+  imports: [
+    ConfigModule.forRoot(),
+    AuthModule,
+    UserModule,
+    MediaModule,
+    SettingsModule,
+    StatisticsModule,
+  ],
+})
+export class AppModule {}
